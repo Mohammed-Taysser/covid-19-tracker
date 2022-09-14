e9b03eb7c5f6a02f4ef304895a8794d8767e556d
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-09-14 23:12:06 +0200

    modify `App` src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './apps/App';
+import App from './App';
 import reportWebVitals from './__test__/reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
