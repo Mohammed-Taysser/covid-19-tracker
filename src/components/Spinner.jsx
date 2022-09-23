0d9a1040f9064438b97af84d45f8667b64b96dbd
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-09-23 14:42:16 +0200

    create `Spinner` component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Spinner = (props) => {
+	const spinnerClassName = `spinner-border text-${props.color} ${
+		props.sm ? 'spinner-border-sm' : ''
+	}`;
+	return (
+		<div className={`text-center ${props.className}`}>
+			<div className={spinnerClassName} role="status">
+				<span className="visually-hidden">{props.text}</span>
+			</div>
+			{props.text && <p className="">{props.text}</p>}
+		</div>
+	);
+};
+
+Spinner.defaultProps = {
+	color: 'aurora',
+};
+
+export default Spinner;
